e24f854a7db8fa66dd9d79f51b0347afea6f3169
by151709129 <mg86837242@protonmail.com>
2023-03-19 21:33:27 +1000

    fix cors

--- config/middlewareConfig.js
+++ config/middlewareConfig.js
@@ -9,8 +9,8 @@ export default function (app) {
   // Express CORS middleware – CORS allows to set which frontend URLs are allowed to access APIs
   app.use(
     cors({
-      // origin: `http://localhost:${constants.VITE_PORT}`,
-      origin: `*`,
+      origin: `http://localhost:${constants.VITE_PORT}`,
+      // origin: `*`,
       credentials: true,
     })
   );
