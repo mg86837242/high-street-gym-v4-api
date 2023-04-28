8f2fa518cbb485b6d033e69e44c495d61dec9b25
by151709129 <mg86837242@protonmail.com>
2023-04-28 11:27:19 +1000

    tweak

--- models/addresses.js
+++ models/addresses.js
@@ -9,7 +9,7 @@ export function getAddressesById(id) {
   return pool.query('SELECT * FROM Addresses WHERE id = ?', [id]);
 }
 
-export function getAddressesByDetails(lineOne, lineTwo, suburb, postcode, state, country) {
+export function getIdenticalAddrByDetails(lineOne, lineTwo, suburb, postcode, state, country) {
   return pool.query(
     'SELECT * FROM Addresses WHERE lineOne = ? AND lineTwo = ? AND suburb = ? AND postcode = ? AND state = ? AND country = ?',
     [lineOne, lineTwo, suburb, postcode, state, country]
