eedc59b1f6b251c9834238b264ca225727e3f4ab
by151709129 <mg86837242@protonmail.com>
2023-04-30 10:34:37 +1000

    adjust notes

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// WARNING: The usage of Prisma ORM is currently in testing phrase, not production-ready
 async function seed() {
   // To be implemented – (1) need to create seed records, and (2) seed scripts
   // @see: https://www.prisma.io/docs/guides/migrate/seed-database
