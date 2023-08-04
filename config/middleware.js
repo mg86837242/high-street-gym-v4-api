24952a9f95619db202495e38c0e1a88ddedcdf76
by151709129 <mg86837242@protonmail.com>
2023-08-04 10:57:07 +1000

    fix sess persistence

--- config/middleware.js
+++ config/middleware.js
@@ -43,7 +43,7 @@ export default function (app) {
       sameSite: 'lax',
     },
     proxy: isProd,
-    domain: 'highstreetgymdemo.space',
+    // domain: 'highstreetgymdemo.space',
     // store: new MysqlStore(mysqlConfig),
   };
 
